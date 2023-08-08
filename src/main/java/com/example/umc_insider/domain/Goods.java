707d4589b406f32184e61262bd9e4d9e5b67f34d
yoondaeng <jyjo522i@gmail.com>
2023-08-08 23:45:40 +0900

    상품 삭제 -ing

--- src/main/java/com/example/umc_insider/domain/Goods.java
+++ src/main/java/com/example/umc_insider/domain/Goods.java
@@ -24,8 +24,6 @@ public class Goods {
     @JoinColumn(name = "markets_id")
     private Markets markets_id; // PK, FK
 
-    // goods zipcode 추가하는 건 어떤가요?
-
     @Column(nullable = false)
     private String title;
 
@@ -64,64 +62,7 @@ public class Goods {
         return this;
     }
 
-
-//    public Goods createGoods(String title, String price, Integer rest, String shelf_life, Users usersId, Markets marketsId, Integer sale, String imageUrl) {
-//        this.title = title;
-//        this.price = price;
-//        this.rest = rest;
-//        this.shelf_life = shelf_life;
-//        this.users_id = usersId;
-//        this.markets_id = marketsId;
-//        this.sale = sale;
-//        this.imageUrl = imageUrl;
-//        this.created_at = new Timestamp(System.currentTimeMillis());
-//        this.updated_at = new Timestamp(System.currentTimeMillis());
-//
-//        return this;
-//    }
-
-    public void setTitle(String title) {
-        this.title = title;
-        this.updated_at = new Timestamp(System.currentTimeMillis());
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-        this.updated_at = new Timestamp(System.currentTimeMillis());
-    }
-
-    public void setRest(Integer rest) {
-        this.rest = rest;
-        this.updated_at = new Timestamp(System.currentTimeMillis());
-    }
-
-    public void setShelf_life(String shelf_life) {
-        this.shelf_life = shelf_life;
-        this.updated_at = new Timestamp(System.currentTimeMillis());
-    }
-
-    public void setUsersId(Users usersId) {
-        this.users_id = usersId;
-        this.updated_at = new Timestamp(System.currentTimeMillis());
-    }
-
-    public void setMarketsId(Markets marketsId) {
-        this.markets_id = marketsId;
-        this.updated_at = new Timestamp(System.currentTimeMillis());
-    }
-
-    public void setSale(Integer sale) {
-        this.sale = sale;
-        this.updated_at = new Timestamp(System.currentTimeMillis());
-    }
-
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-        this.updated_at = new Timestamp(System.currentTimeMillis());
-    }
-
-    public void deleteGoods() {this.rest = 0;}
+    public void deleteGoods() { this.rest = 0; }
 
     public void modifyPrice(String price) { this.price = price; }
 

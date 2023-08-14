0f16adacbb4108d80a634b9355edaac579046eb3
yoondaeng <jyjo522i@gmail.com>
2023-08-14 15:04:16 +0900

    GetGoodsRes goodsId 추가

--- src/main/java/com/example/umc_insider/service/ReviewsService.java
+++ src/main/java/com/example/umc_insider/service/ReviewsService.java
@@ -1,34 +1,34 @@
-package com.example.umc_insider.service;
-
-import com.example.umc_insider.config.BaseException;
-import com.example.umc_insider.config.BaseResponseStatus;
-import com.example.umc_insider.domain.Reviews;
-import com.example.umc_insider.dto.request.PostReviewsReq;
-import com.example.umc_insider.dto.response.PostReviewsRes;
-import com.example.umc_insider.repository.ReviewsRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-//@RequiredArgsConstructor
-@Service
-public class ReviewsService {
-    private final ReviewsRepository reviewsRepository;
-
-    @Autowired
-    public ReviewsService(ReviewsRepository reviewsRepository) {
-        this.reviewsRepository = reviewsRepository;
-    }
-
-    // 후기 등록
-    public PostReviewsRes createReviews(PostReviewsReq postReviewsReq) throws BaseException {
-        try {
-            Reviews reviews = new Reviews();
-            reviews.createReviews(postReviewsReq.getContent(), postReviewsReq.getPoint());
-            reviewsRepository.save(reviews);
-            return new PostReviewsRes(reviews.getContent(), reviews.getPoint());
-        } catch (Exception exception) {
-            throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
-        }
-    }
-}
+//package com.example.umc_insider.service;
+//
+//import com.example.umc_insider.config.BaseException;
+//import com.example.umc_insider.config.BaseResponseStatus;
+//import com.example.umc_insider.domain.Reviews;
+//import com.example.umc_insider.dto.request.PostReviewsReq;
+//import com.example.umc_insider.dto.response.PostReviewsRes;
+//import com.example.umc_insider.repository.ReviewsRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+////@RequiredArgsConstructor
+//@Service
+//public class ReviewsService {
+//    private final ReviewsRepository reviewsRepository;
+//
+//    @Autowired
+//    public ReviewsService(ReviewsRepository reviewsRepository) {
+//        this.reviewsRepository = reviewsRepository;
+//    }
+//
+//    // 후기 등록
+//    public PostReviewsRes createReviews(PostReviewsReq postReviewsReq) throws BaseException {
+//        try {
+//            Reviews reviews = new Reviews();
+//            reviews.createReviews(postReviewsReq.getContent(), postReviewsReq.getPoint());
+//            reviewsRepository.save(reviews);
+//            return new PostReviewsRes(reviews.getContent(), reviews.getPoint());
+//        } catch (Exception exception) {
+//            throw new BaseException(BaseResponseStatus.DATABASE_ERROR);
+//        }
+//    }
+//}

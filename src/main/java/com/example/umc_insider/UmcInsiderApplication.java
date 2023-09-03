18a5dc2e9eb6070a6ff2732620439e61bcab5d3b
yoondaeng <jyjo522i@gmail.com>
2023-09-03 16:33:53 +0900

    fix: KakaoLogin

--- src/main/java/com/example/umc_insider/UmcInsiderApplication.java
+++ src/main/java/com/example/umc_insider/UmcInsiderApplication.java
@@ -2,10 +2,8 @@ package com.example.umc_insider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("com.example.umc_insider.client")
 public class UmcInsiderApplication {
 
 	public static void main(String[] args) {
